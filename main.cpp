a0f8f0068a10c467545eb9224fb070e1941012ce
Mannan Dhillon <74431005+Manasr12@users.noreply.github.com>
2024-03-24 23:24:01 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file main.cpp
+ * @author Mannan Dhillon
+ *
+ * Main entry point for program
+ */
+#include "pch.h"
+#include "CityApp.h"
+
+wxIMPLEMENT_APP(CityApp);
+
--- pch.h
+++ pch.h
@@ -3,14 +3,12 @@
  * @author Mannan Dhillon
  */
 
-#ifndef CITYLIB_PCH_H
-#define CITYLIB_PCH_H
+#ifndef CITY_PCH_H
+#define CITY_PCH_H
 
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
 
-#include <wx/xml/xml.h>
-
-#endif //CITYLIB_PCH_H
+#endif //CITY_PCH_H
